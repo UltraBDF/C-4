0263142c73c1419f849056a9336b1f66eaf6c06a
abourlard <alan.bourlard@etu.u-bordeaux.fr>
2025-07-22 20:10:31 +0200

    First website version

---
+++ src/app/guides/page.tsx
@@ -0,0 +1,9 @@
+// Fichier : src/app/guides/page.tsx
+export default function GuidesPage() {
+    return (
+      <main className="flex min-h-screen flex-col items-center p-24">
+        <h1 className="text-4xl font-bold">Nos Guides</h1>
+        <p className="mt-4">Bientôt ici, la liste de tous nos super guides.</p>
+      </main>
+    );
+  }
